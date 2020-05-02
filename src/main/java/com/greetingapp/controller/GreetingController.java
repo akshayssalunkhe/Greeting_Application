f830fb2e1e56e7566be6ea8901feac9c09d9c37e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-05-02 19:39:07 +0530

    REFACTORED:Code To Return JSON Object

--- src/main/java/com/greetingapp/controller/GreetingController.java
+++ src/main/java/com/greetingapp/controller/GreetingController.java
@@ -1,27 +1,40 @@
 package com.greetingapp.controller;
 
+import com.greetingapp.dto.Greeting;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello %s!";
+
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello";
     }
 
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public String sayHello(@RequestParam(value = "name") String name) {
-        return "Hello" + name;
+    public Greeting sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
     @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name) {
-        return "Hello " + name + "!";
+    public Greeting sayHelloParam(@PathVariable String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
     }
 
     @PutMapping("/put/{firstName}")
-    public String SayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
-        return "Hello " + firstName + " " + lastName + "!";
+    public Greeting greetingPathVariableAndRequestParam(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        String template = "Hello , %s %s !";
+        return new Greeting(counter.incrementAndGet(), String.format(template, firstName, lastName));
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
