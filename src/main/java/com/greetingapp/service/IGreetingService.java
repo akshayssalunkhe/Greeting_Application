2660f014956e656ee7a560f803abf5a6310cd73c
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-05-03 18:09:40 +0530

    ADDED:Abstract Methods

--- src/main/java/com/greetingapp/service/IGreetingService.java
+++ src/main/java/com/greetingapp/service/IGreetingService.java
@@ -2,4 +2,10 @@ package com.greetingapp.service;
 
 public interface IGreetingService {
     String getGreeting();
+
+    String getGreetingWithFirstName(String firstName);
+
+    String getGreetingWithLastName(String lastName);
+
+    String getGreetingWithFullName(String lastName);
 }
